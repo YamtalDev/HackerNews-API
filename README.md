870d36f8edf7f4a3d154a88fd788002aba51ef37
YamtalDev <tal.aharon.work@gmail.com>
2023-09-19 10:40:46 +0300

    Initial commit for the project structure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# WS-RESTFul-API
+# MiniHackerNews-RESTFul-API
 RESTful API for managing simple text news posts, supporting create, update, read, delete, upvote, and downvote operations. 
---
+++ src/main/java/com/akamai/MiniHackerNews/MiniHackerNewsApplication.java
@@ -0,0 +1,13 @@
+package com.akamai.MiniHackerNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniHackerNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniHackerNewsApplication.class, args);
+	}
+
+}
