b7619f50e128f045b0b29350c1058c1d9d2d35a2
YamtalDev <tal.aharon.work@gmail.com>
2023-09-20 14:08:05 +0300

    Updating the new model after thinking again on the requierments

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MiniHackerNews-RESTFul-API
 RESTful API for managing simple text news posts, supporting create, update, read, delete, upvote, and downvote operations. 
+
+
+
+
+If i had more time i would implement the comments count and posting of comments.
+Restrictions for 1 up vote and down vote at a time per user in a session.
--- src/main/java/com/akamai/MiniHackerNews/model/NewsPost.java
+++ src/main/java/com/akamai/MiniHackerNews/model/NewsPost.java
@@ -4,46 +4,43 @@ import lombok.Data;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import jakarta.validation.constraints.*;
-
-/******************************************************************************
- * Represents a news post in the Mini Hacker News system.
- * Each news post contains a title, text, creation time, updated time, and vote counts.
-******************************************************************************/
+import org.hibernate.validator.constraints.URL;
 
 @Data
 @Entity
 @Table(name = "News-Post")
+/******************************************************************************
+ * News post model represents a post for the Mini Hacker News system.
+ * Each news post contains a title, text, creation time, updated time, and vote counts.
+******************************************************************************/
 public class NewsPost
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long post_id;
+    @Max(value = Long.MAX_VALUE, message = "Maximum capacity of posts")
+    private long post_id;
 
     @Column(name = "title", nullable = false)
-    @NotBlank(message = "Title text is required")
-    @Size(min = 1, max = 255, message = "Post text must be between 1 and 255 characters")
+    @NotBlank(message = "Title is required")
+    @Size(min = 1, max = 255, message = "Post title must be between 1 and 255 characters")
     private String title;
 
-    @Column(name = "post", nullable = false)
-    @NotBlank(message = "Post text is required")
-    @Size(min = 1, max = 8096, message = "Post text must be between 1 and 8096 characters")
-    private String text;
-
-    @Column(name = "updated_time")
-    @Past(message = "Updated time must be in the past")
-    private LocalDateTime updatedTime;
+    @Column(name = "user_name", nullable = false)
+    @NotBlank(message = "User name is required")
+    @Size(min = 3, max = 20, message = "User name must be between 3 to 20 characters")
+    private String userName;
 
     @Column(name = "creation_time")
     @PastOrPresent(message = "Creation time must be in the past or present")
     private LocalDateTime creationTime;
 
-    @Column(name = "down_votes")
-    @Min(value = 0, message = "Down votes cannot be negative")
-    @Max(value = 10000, message = "Down votes cannot exceed 10000")
-    private Long downVotes;
+    @Column(name = "votes")
+    @Min(value = 0, message = "Votes must be a non-negative value")
+    @Max(value = Integer.MAX_VALUE, message = "Maximum votes reached")
+    private int votes;
 
-    @Column(name = "up_votes")
-    @Min(value = 0, message = "Up votes cannot be negative")
-    @Max(value = 10000, message = "Up votes cannot exceed 10000")
-    private Long upVotes;
+    @Column(name = "link")
+    @URL(message = "Invalid URL. Please provide a valid HTTP or HTTPS URL.")
+    @Size(min = 10, max = 1024, message = "Link must be between 10 to 1024 characters long")
+    private String link;
 }
