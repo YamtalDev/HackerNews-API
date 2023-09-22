4b4341ec3dad1aa8887f40999f57cd1cd0d7c3c4
YamtalDev <tal.aharon.work@gmail.com>
2023-09-22 17:10:37 +0300

    Updating controllers

--- src/main/java/com/akamai/MiniHackerNews/controller/NewsPostController.java
+++ src/main/java/com/akamai/MiniHackerNews/controller/NewsPostController.java
@@ -1,23 +1,26 @@
 package com.akamai.MiniHackerNews.controller;
 
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
+
+import com.akamai.MiniHackerNews.service.*;
+import com.akamai.MiniHackerNews.schema.dto.*;
+
 import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CacheConfig;
-import com.akamai.MiniHackerNews.service.NewsPostService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import com.akamai.MiniHackerNews.schema.dto.NewsPostRequestDTO;
-import com.akamai.MiniHackerNews.schema.dto.NewsPostResponseDTO;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,12 +32,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @description : 
 
  ******************************************************************************/
-
-// @CrossOrigin
-// Use it if you want to set up front end that fetches the server
-
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/news")
 @CacheConfig(cacheNames = "anato")
 public class NewsPostController
 {
@@ -45,14 +44,15 @@ public class NewsPostController
         this.newsService = newsService;
     }
     
-    @PostMapping(value = "/news")
-    public ResponseEntity<NewsPostResponseDTO> saveNewsPost(@Valid @RequestBody NewsPostRequestDTO newsPost)
+    @PostMapping(value = "")
+    public ResponseEntity<NewsPostResponseDTO> saveNewsPost
+    (@Valid @RequestBody NewsPostRequestDTO newsPost)
     {
         return new ResponseEntity<NewsPostResponseDTO>
         (newsService.saveNewsPost(newsPost), HttpStatus.CREATED);
     }
 
-    @GetMapping("/news")
+    @GetMapping(value = "")
     public ResponseEntity<Page<NewsPostResponseDTO>> getAllPosts(Pageable pageable)
     {
         Page<NewsPostResponseDTO> newsPosts = newsService.getAllPosts(pageable);
@@ -60,53 +60,61 @@ public class NewsPostController
     }
 
     @Cacheable("anato")
-    @GetMapping("/news/top-posts")
+    @GetMapping(value = "/top-posts")
     public ResponseEntity<Page<NewsPostResponseDTO>> getTopPostsByRank(Pageable pageable)
     {
         Page<NewsPostResponseDTO> newsPosts = newsService.getPostsByRankDesc(pageable);
-        return (new ResponseEntity<>(newsPosts, HttpStatus.OK));
+        return (new ResponseEntity<Page<NewsPostResponseDTO>>(newsPosts, HttpStatus.OK));
     }
-    
+
     @Cacheable(key = "#post_id")
-    @GetMapping("/news/{post_id}")
+    @GetMapping(value = "/{postId}")
     public ResponseEntity<NewsPostResponseDTO> getPostById
-    (@Valid @PathVariable("post_id") Long post_id)
+    (@Valid @PathVariable("postId") Long postId)
     {
         return (new ResponseEntity<NewsPostResponseDTO>
-        (newsService.getPostById(post_id), HttpStatus.OK));
+        (newsService.getPostById(postId), HttpStatus.OK));
     }
 
     @CachePut(key = "#post_id")
-    @PutMapping("/news/{post_id}")
+    @PutMapping(value = "/{postId}")
     public ResponseEntity<NewsPostResponseDTO> updatePost
-    (@Valid @RequestBody NewsPostRequestDTO updatedPost, @Valid @PathVariable("post_id") Long post_id)
+    (@Valid @RequestBody NewsPostRequestDTO updatedPost, @Valid @PathVariable("postId") Long postId)
     {
         return (new ResponseEntity<NewsPostResponseDTO>
-        (newsService.updatePost(updatedPost, post_id), HttpStatus.OK));
+        (newsService.updatePost(updatedPost, postId), HttpStatus.OK));
     }
 
     @CacheEvict(key = "#post_id")
-    @DeleteMapping("/news/{post_id}")
+    @DeleteMapping(value = "/{postId}")
     public ResponseEntity<String> deletePost
-    (@Valid @PathVariable("post_id") Long post_id)
+    (@Valid @PathVariable("postId") Long postId)
     {
-        newsService.deletePost(post_id);
+        newsService.deletePost(postId);
         return (new ResponseEntity<String>("Post deleted", HttpStatus.OK));
     }
 
-    @PatchMapping("/news/{post_id}/upvote")
-    public ResponseEntity<String> upvotePost
-    (@Valid @PathVariable("post_id") Long post_id)
+    @PatchMapping(value = "/{post_id}")
+    public ResponseEntity<NewsPostResponseDTO> changePost
+    (@Valid @RequestBody NewsUpdateRequestDTO changedPost, @Valid @PathVariable("postId") Long postId)
     {
-        return (new ResponseEntity<String>
-        (newsService.upvotePost(post_id) + " 👍", HttpStatus.OK));
+        return (new ResponseEntity<NewsPostResponseDTO>
+        (newsService.changePost(changedPost, postId), HttpStatus.OK));
     }
 
-    @PatchMapping("/news/{post_id}/downvote")
-    public ResponseEntity<String> downvotePost
-    (@Valid @PathVariable("post_id") Long post_id)
+    @PatchMapping(value = "/{post_id}/upvote")
+    public ResponseEntity<String> upvotePost
+    (@Valid @PathVariable("postId") Long postId)
     {
         return (new ResponseEntity<String>
-        (newsService.downvotePost(post_id) + " 👎", HttpStatus.OK));
+        ( "👍 " + newsService.upvotePost(postId), HttpStatus.OK));
+    }
+
+    @PatchMapping(value = "/{post_id}/downvote")
+    public ResponseEntity<String> downvotePost
+    (@Valid @PathVariable("postId") Long postId)
+    {
+        return (new ResponseEntity<String>
+        ("👎 " + newsService.downvotePost(postId), HttpStatus.OK));
     }
 }
