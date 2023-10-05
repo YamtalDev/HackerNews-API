681443ec07a2ab7d032e26e4513e6e792fea1ba2
YamtalDev <tal.aharon.work@gmail.com>
2023-10-05 17:32:04 +0300

    Removing the license documentation from the project files

--- src/main/java/com/akamai/MiniHackerNews/schema/NewsPostSchema.java
+++ src/main/java/com/akamai/MiniHackerNews/schema/NewsPostSchema.java
@@ -1,27 +1,3 @@
-/******************************************************************************
-MIT License
-
-Copyright (c) 2023 Tal Aharon
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-******************************************************************************/
 package com.akamai.MiniHackerNews.schema;
 
 import lombok.Data;
@@ -107,15 +83,15 @@ public class NewsPostSchema
     /**************************************************************************
      * @GettersNSetters : Implementation for the ModelMapper to map between DTO entities.
     **************************************************************************/
-    public int getVotes(){return (votes);}
-    public String getPost(){return (post);}
-    public Long getPostId(){return (postId);}
-    public String getPostedBy(){return (postedBy);}
-    public String getTimeElapsed(){return (timeElapsed);}
+    public int getVotes(){ return (votes);}
+    public String getPost(){ return (post);}
+    public Long getPostId(){ return (postId);}
+    public String getPostedBy(){ return (postedBy);}
+    public String getTimeElapsed(){ return (timeElapsed);}
 
-    public void setPost(String post){this.post = post;}
-    public void setLink(String link){this.link = link;}
-    public void setPostedBy(String posted_by){this.postedBy = posted_by;}
+    public void setPost(String post){ this.post = post;}
+    public void setLink(String link){ this.link = link;}
+    public void setPostedBy(String posted_by){ this.postedBy = posted_by;}
 
     /**************************************************************************
      * @Voting: Methods to upvote and downvote the post.
@@ -136,8 +112,6 @@ public class NewsPostSchema
     @PrePersist
     public void setTime()
     {
-        rank = 0;
-        votes = 0;
         timeElapsed = "Just now";
         this.creationTime = LocalDateTime.now();
     }
