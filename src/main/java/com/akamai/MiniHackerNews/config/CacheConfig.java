64f7313860ed965ab17dbc6e6f233a484b5afb48
YamtalDev <tal.aharon.work@gmail.com>
2023-10-06 09:33:08 +0300

    Configuring cache from the properties file externally

--- src/main/java/com/akamai/MiniHackerNews/config/CacheConfig.java
+++ src/main/java/com/akamai/MiniHackerNews/config/CacheConfig.java
@@ -1,12 +1,13 @@
 package com.akamai.MiniHackerNews.config;
 
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CacheConfig
+public class CacheConfig implements CachingConfigurer
 {
     @Bean
     public CacheManager cacheManager()
