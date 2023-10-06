78cd0221d68d8c7248b4e6c901a52f1c0fbd4124
YamtalDev <tal.aharon.work@gmail.com>
2023-10-06 12:34:02 +0300

    Updating initial work on the cache logic

--- src/main/java/com/akamai/MiniHackerNews/service/impl/HackerNewsCacheServiceImpl.java
+++ src/main/java/com/akamai/MiniHackerNews/service/impl/HackerNewsCacheServiceImpl.java
@@ -1,10 +1,12 @@
 package com.akamai.MiniHackerNews.service.impl;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Service;
 
+import com.akamai.MiniHackerNews.dto.NewsPostResponseDTO;
 import com.akamai.MiniHackerNews.service.HackerNewsCacheService;
 
 @Service
@@ -23,9 +25,26 @@ public class HackerNewsCacheServiceImpl implements HackerNewsCacheService
         return (cache.get(key));
     }
 
+
     @Override
     public void put(String key, Object value)
     {
+        List<?> topPosts = (List<?>)value;
+
+        for(int i = 0; i < topPosts.size(); i++)
+        {
+            Object listItem = topPosts.get(i);
+            if(listItem instanceof NewsPostResponseDTO)
+            {
+                NewsPostResponseDTO post = (NewsPostResponseDTO) listItem;
+                if (key.equals(String.valueOf(post.getPostId())))
+                {
+                    topPosts.set(i, value);
+                    break;
+                }
+            }
+        }
+
         cache.put(key, value);
     }
 
